9f406a9cb7ee837a196d1f296a05322d0d1c2ea4
hascherkaoui <Hassan.cherkaoui@cloudlink.us>
2022-07-25 14:45:20 +0100

    fixed Collections errors

--- components/Layout/DefaultLayout.tsx
+++ components/Layout/DefaultLayout.tsx
@@ -34,7 +34,7 @@ export default function DefaultLayout(props: DefaultLayoutProps) {
 
       <div className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow navbar-style">
         <Link href="/">
-          <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3">Rekognition IdV</a>
+          <a className="navbar-brand col-md-3 col-lg-2 me-0 px-2">GLOWSTIK Id Verification</a>
         </Link>
         <button className="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
